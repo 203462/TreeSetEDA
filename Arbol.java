43cbcbeddaecab7335a30eafc2cc383f650a838c
203462 <203462@ids.upchiapas.edu.mx>
2021-07-03 19:27:09 -0500

    cambios

--- Arbol.java
+++ Arbol.java
@@ -3,9 +3,25 @@ import java.util.*;
 public class Arbol {
     static Scanner entrada = new Scanner(System.in);
 
-    public void ingresar(TreeSet<Alumno> ts) {
-
+    public void ingresar(TreeSet<Alumno> treeset) {
+        int cant;
+        int matricula;
+        int calificacion;
+        System.out.println("Cantidad de datos a ingresar");
+        cant = entrada.nextInt();
+        for (int i = 0; i < cant; i++) {
+            System.out.println("Ingrese la matricula: ");
+            matricula = entrada.nextInt();
+            System.out.println("Ingrese la calificacion: ");
+            calificacion = entrada.nextInt();
+            Alumno alumno = new Alumno(matricula, calificacion);
+            treeset.add(alumno);
+        }
     }
+
+    public void
+
+    boolean validar
 
     public boolean buscarAlumno(TreeSet<Alumno> treeset, Alumno alumnoBuscar) {
         boolean validador;
@@ -18,5 +34,34 @@ public class Arbol {
         for (Iterator<Alumno> iterador = treeset.iterator(); iterador.hasNext();) {
             System.out.println(iterador.next());
         }
+    }
+
+    public void darDeBaja(TreeSet<Alumno> treeset, Alumno alumno) {
+        treeset.remove(alumno);
+    }
+
+    public void verAlumnoCalificacion(TreeSet<Alumno> treeset, int matricula) {
+        System.out.println("Matricula Calificacion");
+        matricula = matricula * 1000;
+        int matriculaLimite = matricula + 1000;
+        for (Iterator<Alumno> iterador = treeset.iterator(); iterador.hasNext();) {
+            String datos[] = iterador.next().toString().split(" ");
+            int matriculaAlumno = Integer.parseInt(datos[0]);
+            if (matriculaAlumno >= matricula && matriculaAlumno < matriculaLimite) {
+                System.out.println(datos[0] + " " + datos[1]);
+            }
+        }
+    }
+
+    public void verAlumnoMatricula(TreeSet<Alumno> treeset, int calificacionBuscar) {
+        System.out.println("Matricula Calificacion");
+        for (Iterator<Alumno> iterador = treeset.iterator(); iterador.hasNext();) {
+            String datos[] = iterador.next().toString().split(" ");
+            int calificacionAlumno = Integer.parseInt(datos[1]);
+            if (calificacionAlumno <= calificacionBuscar) {
+                System.out.println(datos[0] + " " + datos[1]);
+            }
+        }
+        System.out.println();
     }
 }
--- Main.java
+++ Main.java
@@ -7,28 +7,40 @@ public class Main {
         int opc;
         Arbol arbol = new Arbol();
         TreeSet<Alumno> treeset = new TreeSet<Alumno>();
-        do{
-            System.out.println("1. Agregar información de un alumnos\n2. Buscar a un alumno\n3. Visualizar lista de alumnos \4. Dar de baja a un alumno\5. Visualizar alumnos por calificacion\6.Visualizar alumnos por matricula\n7. Salir");
-            opc= entrada.nextInt();
-            switch(opc){
+        do {
+            System.out.println(
+                    "1. Agregar información de un alumnos\n2. Buscar a un alumno\n3. Visualizar lista de alumnos \n4. Dar de baja a un alumno\n5. Visualizar alumnos por calificacion\n6.Visualizar alumnos por matricula\n7. Salir");
+            opc = entrada.nextInt();
+            switch (opc) {
                 case 1:
-                arbol.ingresar(treeset);
-                break;
+                    arbol.ingresar(treeset);
+                    break;
                 case 2:
-                Alumno alumnoBuscar = datosDeAlumno();
-                System.out.println("Alumno encontrado en el sistema: " +arbol.buscarAlumno(treeset, alumnoBuscar));
-                break;
+                    Alumno alumnoBuscar = datosDeAlumno();
+                    System.out.println("Alumno encontrado en el sistema: " + arbol.buscarAlumno(treeset, alumnoBuscar));
+                    break;
                 case 3:
-                arbol.visualizarAlumnos(treeset);
-                break;
-
+                    arbol.visualizarAlumnos(treeset);
+                    break;
                 case 4:
-
-                break;
-
+                    Alumno alumnoDarDeBaja = datosDeAlumno();
+                    arbol.darDeBaja(treeset, alumnoDarDeBaja);
+                    break;
                 case 5:
+                    System.out.println("Ingrese la calificacion: ");
+                    int calificacion = entrada.nextInt();
+                    arbol.verAlumnoCalificacion(treeset, calificacion);
+                    break;
+                case 6:
+                    int matricula;
+                    do {
+                        System.out.println("Ingrese los tres primeros digitos de la matricula: ");
+                        matricula = entrada.nextInt();
+                    } while (matricula < 99 || matricula > 1000);
+                    arbol.verAlumnoMatricula(treeset, matricula);
+                    break;
             }
-        }
+        } while (opc != 7);
 
     }
 
